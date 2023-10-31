4d5e06e400a15c5b54305a34e4814e433671528a
Macarena Cardenas Ponce <macarenacp1220@gmail.com>
2023-10-30 23:46:37 -0300

    se añade variables let const var, expresiones mat

---
+++ Fundamentos/seccióń-3-introducción-a-javascript/variables.js
@@ -0,0 +1,47 @@
+const base = 5;
+const height = 8;
+const area = base * height;
+console.log(area);
+
+const perimeter = base + base + height + height;
+console.log(perimeter);
+
+
+// para sacar los cálculos de un rectángulo. 
+
+
+const num1 = 10;
+const num2 = 15;
+
+const resultadoSuma = num1 + num2;
+console.log(resultadoSuma);
+
+
+const resultadoMulti = num1 * num2;
+console.log(resultadoMulti);
+
+const resultadoDivi = num1 / num2;
+console.log(resultadoDivi);
+
+const resultadoModulo = num1 % num2;
+console.log(resultadoModulo);
+
+
+// para calcular las diferentes expresiones matemáticas jijji 
+
+
+const number1 = 30;
+const number2 = 45;
+let message;
+
+if (number1 > number2) { 
+   console.log('este es el número mayor:',number1)
+}
+
+else {
+    console.log('este es el número mayor:',number2);
+}
+
+// para mostrar el resultado del numero mayor 
+
+
